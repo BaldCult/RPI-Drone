02ead42f5bb3ff311ac8b871fe0ab9d82cf89453
tumothy2913 <159853735+tumothy2913@users.noreply.github.com>
2025-03-20 14:11:07 -0400

    Update controller.py

--- controller.py
+++ controller.py
@@ -12,10 +12,6 @@ async def left_joystick(gamepad):
                 print(f"Left Joystick X: {event.value}")
             elif event.code == ecodes.ABS_Y:
                 print(f"Left Joystick Y: {event.value}")
-            elif event.code == ecodes.ABS_Z:
-                print(f"Right Joystick X: {event.value}")
-            elif event.code == ecodes.ABS_RZ:
-                print(f"Right Joystick Y: {event.value}")
 
 async def right_joystick(gamepad):
     async for event in gamepad.async_read_loop():
