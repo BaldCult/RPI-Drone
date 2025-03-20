2ce0f1b69513ad37715b89d5db3c39fdef4fd2f3
tumothy2913 <159853735+tumothy2913@users.noreply.github.com>
2025-03-20 14:03:47 -0400

    Update controller.py

--- controller.py
+++ controller.py
@@ -16,17 +16,5 @@ async def main(gamepad):
                 print(f"Right Joystick X: {event.value}")
             elif event.code == ecodes.ABS_RZ:
                 print(f"Right Joystick Y: {event.value}")
-        #print(repr(ev))
 
 asyncio.run(main(gamepad))
-
- #for event in gamepad.read_loop():
-    #if event.type == ecodes.EV_ABS:
-        #if event.code == ecodes.ABS_X:
-            #print(f"Left Joystick X: {event.value}")
-        #elif event.code == ecodes.ABS_Y:
-            #print(f"Left Joystick Y: {event.value}")
-        #elif event.code == ecodes.ABS_Z:
-            #print(f"Right Joystick X: {event.value}")
-        #elif event.code == ecodes.ABS_RZ:
-            #print(f"Right Joystick Y: {event.value}") 
