c1c0877b4ac0d295a1f15d84e7a5cf94dfa02ae5
tumothy2913 <159853735+tumothy2913@users.noreply.github.com>
2025-03-20 13:50:39 -0400

    Update and rename controller_code_better.py to controller.py

--- controller.py
+++ controller.py
@@ -6,7 +6,7 @@ gamepad = InputDevice('/dev/input/event6')
 print(gamepad)
 
 async def main(gamepad):
-    async for ev in gamepad.async_read_loop():
+    async for event in gamepad.async_read_loop():
         if event.type == ecodes.EV_ABS:
             if event.code == ecodes.ABS_X:
                 print(f"Left Joystick X: {event.value}")
@@ -20,13 +20,13 @@ async def main(gamepad):
 
 asyncio.run(main(gamepad))
 
-for event in gamepad.read_loop():
-    if event.type == ecodes.EV_ABS:
-        if event.code == ecodes.ABS_X:
-            print(f"Left Joystick X: {event.value}")
-        elif event.code == ecodes.ABS_Y:
-            print(f"Left Joystick Y: {event.value}")
-        elif event.code == ecodes.ABS_Z:
-            print(f"Right Joystick X: {event.value}")
-        elif event.code == ecodes.ABS_RZ:
-            print(f"Right Joystick Y: {event.value}")
+ #for event in gamepad.read_loop():
+    #if event.type == ecodes.EV_ABS:
+        #if event.code == ecodes.ABS_X:
+            #print(f"Left Joystick X: {event.value}")
+        #elif event.code == ecodes.ABS_Y:
+            #print(f"Left Joystick Y: {event.value}")
+        #elif event.code == ecodes.ABS_Z:
+            #print(f"Right Joystick X: {event.value}")
+        #elif event.code == ecodes.ABS_RZ:
+            #print(f"Right Joystick Y: {event.value}") 
