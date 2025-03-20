7b39a2cd8d93e0b6ac13f8d6d682ced479c50f19
tumothy2913 <159853735+tumothy2913@users.noreply.github.com>
2025-03-20 14:09:03 -0400

    Update controller.py

--- controller.py
+++ controller.py
@@ -5,7 +5,7 @@ gamepad = InputDevice('/dev/input/event6')
 
 print(gamepad)
 
-async def main(gamepad):
+async def left_joystick(gamepad):
     async for event in gamepad.async_read_loop():
         if event.type == ecodes.EV_ABS:
             if event.code == ecodes.ABS_X:
@@ -17,4 +17,15 @@ async def main(gamepad):
             elif event.code == ecodes.ABS_RZ:
                 print(f"Right Joystick Y: {event.value}")
 
-asyncio.run(main(gamepad))
+async def right_joystick(gamepad):
+    async for event in gamepad.async_read_loop():
+        if event.type == ecodes.EV_ABS:
+            if event.code == ecodes.ABS_Z:
+                print(f"Right Joystick X: {event.value}")
+            elif event.code == ecodes.ABS_RZ:
+                print(f"Right Joystick Y: {event.value}")
+
+async def main():
+    await asyncio.gther(left_joystick(gamepad), right_joystick(gamepad))
+
+asyncio.run(main())
