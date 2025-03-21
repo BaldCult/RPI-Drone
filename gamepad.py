0f50009430b4e4e71f3fb6487fb3ba0b602dabc2
tumothy2913 <159853735+tumothy2913@users.noreply.github.com>
2025-03-21 13:53:08 -0400

    Update gamepad.py
    Testing finding device dynaically by name

--- gamepad.py
+++ gamepad.py
@@ -2,5 +2,7 @@ import evdev
 import asyncio
 from evdev import InputDevice, categorize, ecodes
 
-gamepad = InputDevice('/dev/input/event5')
-print(gamepad)
+for device_path in evdev.list_devices():
+  device = evdev.InputDevice(device_path)
+        # Match the device name exactly or partially
+  print(device.name)
