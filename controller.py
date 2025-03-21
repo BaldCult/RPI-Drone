6470a53add163dd987d324af648ebce184f80fa4
tumothy2913 <159853735+tumothy2913@users.noreply.github.com>
2025-03-21 13:35:21 -0400

    Update controller.py

--- controller.py
+++ controller.py
@@ -4,7 +4,7 @@ from evdev import InputDevice, categorize, ecodes
 def find_gamepad_device():
     for device_path in evdev.list_devices():
         device = evdev.InputDevice(device_path)
-        if 'event' in device.path and 'gamepad' in device.name.lower():
+        if 'event' in device.path and 'xbox wireless controller' in device.name.lower():
             print(f"Found gamepad: {device.path} - {device.name}")
             return device
     raise Exception("No gamepad found")
