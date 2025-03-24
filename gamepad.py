20630fca234ad675c859738df31e3fb6326f3e8c
tumothy2913 <159853735+tumothy2913@users.noreply.github.com>
2025-03-24 12:24:53 -0400

    Delete gamepad.py

--- gamepad.py
+++
@@ -1,8 +0,0 @@
-import evdev
-import asyncio
-from evdev import InputDevice, categorize, ecodes
-
-for device_path in evdev.list_devices():
-  device = evdev.InputDevice(device_path)
-        # Match the device name exactly or partially
-  print(device.name)
