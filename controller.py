85ea254991e40acb65d83243ce2e901ee636ed94
BaldCult <112122534+BaldCult@users.noreply.github.com>
2025-03-28 12:39:46 -0400

    Update controller.py

--- controller.py
+++ controller.py
@@ -26,6 +26,7 @@ async def joysticks(gamepad):
     right_Y = 0
     
     async for event in gamepad.async_read_loop():  
+        GpioPins = [4, 17, 27, 22]
         mymotortest.motor_run(GpioPins,.05,left_Y, True, True,"full", .05)
         if event.type == ecodes.EV_ABS:
             if event.code == ecodes.ABS_X:
@@ -53,7 +54,6 @@ async def joysticks(gamepad):
      #           print(f"Right Joystick Y: {event.value}")
 
 async def main():
-    GpioPins = [4, 17, 27, 22]
     gamepad = find_gamepad_device()
     
     await analog_inputs(gamepad)
