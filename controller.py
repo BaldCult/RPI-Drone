75d452b28523f23b3b186e919aba3b58a6a08d25
tumothy2913 <159853735+tumothy2913@users.noreply.github.com>
2025-03-28 12:17:23 -0400

    Update controller.py

--- controller.py
+++ controller.py
@@ -18,15 +18,22 @@ def normalize(value):
 
 async def joysticks(gamepad):
     async for event in gamepad.async_read_loop():
+        left_X = 0, left_Y = 0, right_X = 0, right_Y = 0
+        
         if event.type == ecodes.EV_ABS:
             if event.code == ecodes.ABS_X:
-                print(f"Left Joystick X: {normalize.event.value}") #event.code == ecodes.ABS_X
+                left_X = normalize.(event.value)
             elif event.code == ecodes.ABS_Y:
-                print(f"Left Joystick Y: {normalize.event.value}") #event.code == ecodes.ABS_Y
+                left_Y = normalize.(event.value)
             elif event.code == ecodes.ABS_Z:
-                print(f"Right Joystick X: {normalize.event.value}") #event.code == ecodes.ABS_Z
+                right_X = normalize.(event.value)
             elif event.code == ecodes.ABS_RZ:
-                print(f"Right Joystick Y: {normalize.event.value}") #event.code == ecodes.ABS_RZ
+                right_Y = normalize.(event.value)
+
+        print(f"Left Joystick X: {left_X}") #event.code == ecodes.ABS_X
+        print(f"Left Joystick Y: {left_Y}") #event.code == ecodes.ABS_Y
+        print(f"Right Joystick X: {right_X}") #event.code == ecodes.ABS_Z
+        print(f"Right Joystick Y: {right_Y}") #event.code == ecodes.ABS_RZ
             
 
 #async def right_joystick(gamepad):
