15844193657c06392841e3b7419efb64a5b721ec
tumothy2913 <159853735+tumothy2913@users.noreply.github.com>
2025-04-17 13:36:51 -0400

    Update telemetry.py

--- telemetry.py
+++ telemetry.py
@@ -8,7 +8,6 @@ mpu = adafruit_mpu6050.MPU6050(i2c)
 
 while True:
     accTuple = (mpu.acceleration[0], mpu.acceleration[1], ((float)(mpu.acceleration[2]))+2.0)
-    #accTuple[2] = ((float)(accTuple[2]))-2.0
 
     print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (accTuple))
     print("Gyro X:%.2f, Y: %.2f, Z: %.2f rad/s" % (mpu.gyro))
