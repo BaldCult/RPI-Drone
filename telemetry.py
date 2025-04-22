ce41d035c03d315803416ee1bfd41ba99570baa3
BaldCult <112122534+BaldCult@users.noreply.github.com>
2025-04-22 12:55:01 -0400

    Update telemetry.py

--- telemetry.py
+++ telemetry.py
@@ -1,6 +1,7 @@
 import time
 import board
 import adafruit_mpu6050
+import asyncio
 
 i2c = board.I2C()  # uses board.SCL and board.SDA
 # i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
@@ -15,9 +16,9 @@ mpu = adafruit_mpu6050.MPU6050(i2c)
    # print("")
     #time.sleep(1)
 
-def get_gyro_data():
-    return {
+async def get_gyro_data():
+    while true
+        accTuple = (mpu.acceleration[0], mpu.acceleration[1], ((float)(mpu.acceleration[2]))+2.0)
         "acceleration": {"x": mpu.acceleration[0], "y": mpu.acceleration[1], "z": ((float)(mpu.acceleration[2]))+2.0},
         "gyro": {"x": mpu.gyro[0], "y": mpu.gyro[1], "z": mpu.gyro[2]},
         "temperature": mpu.temperature
-    }
