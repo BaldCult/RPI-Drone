6c877d30becf8e34967c7d684b0d625c822b5e45
BaldCult <112122534+BaldCult@users.noreply.github.com>
2025-04-22 12:43:51 -0400

    Update telemetry.py

--- telemetry.py
+++ telemetry.py
@@ -6,11 +6,18 @@ i2c = board.I2C()  # uses board.SCL and board.SDA
 # i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
 mpu = adafruit_mpu6050.MPU6050(i2c)
 
-while True:
-    accTuple = (mpu.acceleration[0], mpu.acceleration[1], ((float)(mpu.acceleration[2]))+2.0)
+#while True:
+ #   accTuple = (mpu.acceleration[0], mpu.acceleration[1], ((float)(mpu.acceleration[2]))+2.0)
 
-    print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (accTuple))
-    print("Gyro X:%.2f, Y: %.2f, Z: %.2f rad/s" % (mpu.gyro))
-    print("Temperature: %.2f C" % mpu.temperature)
-    print("")
-    time.sleep(1)
+#    print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (accTuple))
+ #   print("Gyro X:%.2f, Y: %.2f, Z: %.2f rad/s" % (mpu.gyro))
+  #  print("Temperature: %.2f C" % mpu.temperature)
+   # print("")
+    #time.sleep(1)
+
+def get_gyro_data():
+    return {
+        "acceleration": {"x": mpu.acceleration[0], "y": mpu.acceleration[1], "z": ((float)(mpu.acceleration[2]))+2.0},
+        "gyro": {"x": mpu.gyro[0], "y": mpu.gyro[1], "z": mpu.gyro[2]},
+        "temperature": mpu.temperature
+    }
