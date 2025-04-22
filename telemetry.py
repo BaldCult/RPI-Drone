f852160cf1a6629992839fedb0a22fcaca08cf0b
BaldCult <112122534+BaldCult@users.noreply.github.com>
2025-04-22 12:57:21 -0400

    Update telemetry.py

--- telemetry.py
+++ telemetry.py
@@ -18,7 +18,9 @@ mpu = adafruit_mpu6050.MPU6050(i2c)
 
 async def get_gyro_data():
     while true
-        accTuple = (mpu.acceleration[0], mpu.acceleration[1], ((float)(mpu.acceleration[2]))+2.0)
-        "acceleration": {"x": mpu.acceleration[0], "y": mpu.acceleration[1], "z": ((float)(mpu.acceleration[2]))+2.0},
-        "gyro": {"x": mpu.gyro[0], "y": mpu.gyro[1], "z": mpu.gyro[2]},
-        "temperature": mpu.temperature
+         accTuple = (mpu.acceleration[0], mpu.acceleration[1], ((float)(mpu.acceleration[2]))+2.0)
+         sensor_data = {
+            "acceleration": {"x": accTuple[0], "y": accTuple[1], "z": accTuple[2]},
+            "gyro": {"x": mpu.gyro[0], "y": mpu.gyro[1], "z": mpu.gyro[2]},
+            "temperature": mpu.temperature
+        }
