4024cf8f0c791b1f9b0f1e13bd0dc552fb3ae420
BaldCult <112122534+BaldCult@users.noreply.github.com>
2025-04-25 12:37:16 -0400

    Update mjpeg_servertest.py

--- mjpeg_servertest.py
+++ mjpeg_servertest.py
@@ -19,7 +19,6 @@ PAGE = """\
 <html>
 <head>
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="style.css">
 <title>RPI-Drone</title>
 </head>
