667833c1903987ccc127ae89bd60c01fb3839614
tumothy2913 <159853735+tumothy2913@users.noreply.github.com>
2025-05-29 10:44:30 -0400

    Create PWM_Test.py

---
+++ PWM_Test.py
@@ -0,0 +1,17 @@
+import RPI.GPIO as GPIO
+from time import sleep
+
+signal_pin = 12
+
+def pinSetup() {
+  GPIO.setwarnings(False)
+  GPIO.setmode(GPIO.BOARD)
+  GPIO.setup(signal_pin, GPIO.OUT)
+}
+
+pinSetup()
+
+esc_pwm = GPIO.PWM(signal_pin, 50)
+esc_pwm.start(7.5)
+sleep(2)
+esc_pwm.stop()
