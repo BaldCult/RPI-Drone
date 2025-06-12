2bc08c07c02f35480cf92b614dd48ca07dde7fce
tumothy2913 <159853735+tumothy2913@users.noreply.github.com>
2025-06-12 13:39:29 -0400

    Update throttle_test.py

--- throttle_test.py
+++ throttle_test.py
@@ -33,23 +33,21 @@ def find_gamepad_device():
 def normalize(value):
     return (value - 32768) / 32768.0  # Normalize to range -1 to 1
 
-async def read_joystick_values():
-    gamepad = find_gamepad_device()
-    async for event in gamepad.async_read_loop():
-        if event.type == ecodes.EV_ABS:
-            if event.code == ecodes.ABS_X:
-                latest_values["left_X"] = round(normalize(event.value), 2)
-            elif event.code == ecodes.ABS_Y:
-                latest_values["left_Y"] = round(normalize(event.value), 2)
-                if event.value in range (-32767, 32767):
-                    pi.set_servo_pulsewidth(ESC_GPIO1, event.value)
-                    pi.set_servo_pulsewidth(ESC_GPIO2, event.value)
-                    pi.set_servo_pulsewidth(ESC_GPIO3, event.value)
-                    pi.set_servo_pulsewidth(ESC_GPIO4, event.value)
-            elif event.code == ecodes.ABS_Z:
-                latest_values["right_X"] = round(normalize(event.value), 2)
-            elif event.code == ecodes.ABS_RZ:
-                latest_values["right_Y"] = round(normalize(event.value), 2)
-
 while true:
-    read_joystick_values()
+    async def read_joystick_values():
+        gamepad = find_gamepad_device()
+        async for event in gamepad.async_read_loop():
+            if event.type == ecodes.EV_ABS:
+                if event.code == ecodes.ABS_X:
+                    latest_values["left_X"] = round(normalize(event.value), 2)
+                elif event.code == ecodes.ABS_Y:
+                    latest_values["left_Y"] = round(normalize(event.value), 2)
+                    if event.value in range (-32767, 32767):
+                        pi.set_servo_pulsewidth(ESC_GPIO1, event.value)
+                        pi.set_servo_pulsewidth(ESC_GPIO2, event.value)
+                        pi.set_servo_pulsewidth(ESC_GPIO3, event.value)
+                        pi.set_servo_pulsewidth(ESC_GPIO4, event.value)
+                elif event.code == ecodes.ABS_Z:
+                    latest_values["right_X"] = round(normalize(event.value), 2)
+                elif event.code == ecodes.ABS_RZ:
+                    latest_values["right_Y"] = round(normalize(event.value), 2)
